6ec20de3b80107296de5bdf69b33c9bcb52ad2f5
kumaramit31 <rawatamit7431@gmail.com>
2025-05-29 02:13:32 +0530

    schedule component creation

--- cumun/src/App.jsx
+++ cumun/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Hero from './components/Hero';
 import ImageStack from './components/ImageStack';
 import Registration from './components/Registration';
+import Schedule from './components/schedule';
 
 const App = () => {
   return (
       <div className="font-sans bg-gray-50">
-        <Hero/>
+        <Hero/> 
         <ImageStack/>
         <Registration/>
+        <Schedule/>
         hello
       </div>
   );
