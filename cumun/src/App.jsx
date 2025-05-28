f16bfff153aee01f146fecaae9b1cb53db472d2b
Sahil <sahil06.wadhwa@gmail.com>
2025-05-29 02:33:38 +0530

    bg_commit2

--- cumun/src/App.jsx
+++ cumun/src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Hero from './components/Hero';
 import ImageStack from './components/ImageStack';
 import Registration from './components/Registration';
+import Schedule from './components/Schedule';
+import VantaBackground from './components/VantaBackground';
 
 const App = () => {
   return (
       <div className="font-sans bg-gray-50">
         <Hero/>
+        <VantaBackground/>
+        <Schedule/>
         <ImageStack/>
         <Registration/>
         <Schedule/>
